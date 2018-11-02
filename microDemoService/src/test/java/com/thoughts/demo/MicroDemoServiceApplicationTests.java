08b171d57d59972c156258a0ee80f09c323f45e9
MGR <mardubudi@Govinds-MacBook-Air.local>
2018-11-02 16:50:24 +0530

    MGR : Initial structure commit
    MGR : Initial structure commit

---
+++ microDemoService/src/test/java/com/thoughts/demo/MicroDemoServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thoughts.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroDemoServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
